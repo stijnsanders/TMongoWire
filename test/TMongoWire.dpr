7559e7318700b25b8e982940efa59d680f4a1525
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2013-10-24 01:07:32 -0200

    Included new tests

--- test/TMongoWire.dpr
+++ test/TMongoWire.dpr
@@ -1,14 +1,5 @@
 program TMongoWire;
-{
 
-  Delphi DUnit Test Project
-  -------------------------
-  This project contains the DUnit test framework and the GUI/Console test runners.
-  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
-  to use the console test runner.  Otherwise the GUI test runner will be used by
-  default.
-
-}
 
 {$IFDEF CONSOLE_TESTRUNNER}
 {$APPTYPE CONSOLE}
--- test/TestBSONUtils.pas
+++ test/TestBSONUtils.pas
@@ -4,208 +4,174 @@ interface
 
 uses
   Dialogs,
-  TestFramework;
+  TestFramework,
+
+  bsonDoc,
+  bsonUtils;
 
 type
 
-///
-/// Include JSON data format entre {* *} in sample JSON bacauuse comflit with { } coments DElphi
-///
-
+  ///
+  /// Include JSON data format entre {* *} in sample JSON bacauuse comflit with { } coments DElphi
+  ///
 
   // test methods bsonUtils.pas
-  TTestBSONUtils = class( TTestCase )
+  TTestBSONUtils = class(TTestCase)
+  private
+    d: IBSONDocument;
+    JSON: string;
   published
     procedure TestBasic();
+
+    procedure Test02();
+    procedure Test03();
+    procedure Test04();
+    procedure Test05();
   end;
 
 implementation
 
 uses
-  Variants,
-
-  bsonDoc, bsonUtils;
+  Variants;
 
 { TTestBSONUtils }
 
-procedure TTestBSONUtils.TestBasic;
-var
-  d: IBSONDocument;
-
-
+procedure TTestBSONUtils.Test02;
 begin
-(*
-{
-   "id":"ObjectID(\"526735c535f5d5052013925f\")",
-   "name":"Johni Douglas Marangon",
-   "address":"Maravilia - Santa Catarina - Brazil",
-   "phone":"4895959859"
-}
-*)
-
   d:= BSON([
-    'name' , 'Johni Douglas Marangon',
-    'address' , 'Maravilia - Santa Catarina - Brazil',
-    'phone' , '4895959859'
-  ] );
+    'Company','XYZ Company',
+    'Address','XYZ Address',
+    'Phones',VarArrayOf([
+      BSON(['number','714-999-9999', 'type','business']),
+      BSON(['number','714-987-6533', 'type','cell'])
+    ])]);
 
+  JSON:=
+    '{"Company":"XYZ Company",' +
+    '"Address":"XYZ Address",' +
+    '"Phones":[' +
+      '{"number":"714-999-9999","type":"business"},' +
+      '{"number":"714-987-6533","type":"cell"}'+
+    ']}';
 
-  d:= BSON([
-  'Company','XYZ Company',
-  'address','XYZ Address',
-  'Phones',VarArrayOf([
-    BSON(['number','714-999-9999', 'type','business']),
-    BSON(['number','714-987-6533', 'type','cell'])
-  ])]);
+  CheckEqualsString( JSON, BsonToJson( d ) );
+end;
 
+procedure TTestBSONUtils.Test03;
+begin
   d:= BSON([ 'movies' ,
     VarArrayOf( [
-      BSON([ 'title', 'Shut Up and Play the Hits' , 'year ', '2012' ]),
-      BSON([ 'title', 'John Morre no Final' , 'year ', '2012' ]),
-      BSON([ 'title', 'The Comedy' , 'year ', '2012' ]),
-      BSON([ 'title', 'Bem-vindo aos 40 ' , 'year ', '2012' ]),
-      BSON([ 'title', 'Cop Land' , 'year ', '1997' ])
+      BSON([ 'title', 'Shut Up and Play the Hits' , 'year', 2012 ]),
+      BSON([ 'title', 'John Morre no Final' , 'year', 2012 ]),
+      BSON([ 'title', 'The Comedy' , 'year', 2012 ]),
+      BSON([ 'title', 'Bem-vindo aos 40' , 'year', 2012 ]),
+      BSON([ 'title', 'Cop Land' , 'year', 1997])
     ])
   ]);
-  ShowMessage( BsonToJson( d ) );
 
-//  http://www.jsonexample.com/
-
- (*
-  newObject = {
- "first": "John",
- "last": "Doe",
- "age": 39,
- "sex": "M",
- "salary": 70000,
- "registered": true,
- "interests": [ "Reading", "Mountain Biking", "Hacking" ]
-}
-*)
+  JSON:=
+    '{"movies":[' +
+       '{"title":"Shut Up and Play the Hits","year":2012},' +
+       '{"title":"John Morre no Final","year":2012},' +
+       '{"title":"The Comedy","year":2012},' +
+       '{"title":"Bem-vindo aos 40","year":2012},' +
+       '{"title":"Cop Land","year":1997}' +
+    ']}';
 
 
-
-
-newObject = {
- "first": "John",
- "last": "Doe",
- "age": 39,
- "sex": "M",
- "salary": 70000,
- "registered": true,
- "favorites": {
-  "color": "Blue",
-  "sport": "Soccer",
-  "food": "Spaghetti"
- }
-}
-
-  d:= BSON([
-    'firstName' ,  'Johni' ,
-    'address' , 'Maravilia',
-    'salay' , '1000',
-    'actived' , 'true',
-    'sex' , 'M',
-
-    'favorits' , VarArrayOf
-
-  ])
-
-
-
-
-newObject = {
- "first": "John",
- "last": "Doe",
- "age": 39,
- "sex": "M",
- "salary": 70000,
- "registered": true,
- "interests": [ "Reading", "Mountain Biking", "Hacking" ],
- "favorites": {
-  "color": "Blue",
-  "sport": "Soccer",
-  "food": "Spaghetti"
- },
- "skills": [
-  {
-   "category": "PHP",
-   "tests": [
-    { "name": "One", "score": 90 },
-    { "name": "Two", "score": 96 }
-   ]
-  },
-  {
-   "category": "CouchDB",
-   "tests": [
-    { "name": "One", "score": 32 },
-    { "name": "Two", "score": 84 }
-   ]
-  },
-  {
-   "category": "Node.js",
-   "tests": [
-    { "name": "One", "score": 97 },
-    { "name": "Two", "score": 93 }
-   ]
-  }
- ]
-}
-
-
-
-//var
-//  d:IBSONDocument;
-d:=BSON([
-    'id',mongoObjectID,
-    'sentence', 'this is a sentece',
-    'tags','["some", "indexing", "words"]'
-]);
-FMongoWire.Insert(theCollection,d);
-
-
-//var
-//q:TMongoWireQuery;
-//qb:IBSONDocument
-qb:=BSON(['tags', '"words"']); //***
-q:=TMongoWireQuery.Create(FMongoWire);
-q.Query(mwx2Collection, qb); //***
-
-
-d:=BSON([
-    'id',mongoObjectID,
-    'sentence', 'this is a sentece',
-    'tags',VarArrayOf(['some', 'indexing', 'words'])
-]);
-FMongoWire.Insert(theCollection,d);,
-
-
-
-
-
-
-
-
-*)
+  CheckEqualsString( JSON, BsonToJson( d ) );
 
 end;
 
+procedure TTestBSONUtils.Test04;
+begin
+  d:=BSON([
+    'Name', 'Johni',
+    'Sports', VarArrayOf( ['football', 'volleyball', 'chess'] )
+  ]);
+
+  JSON:=
+    '{"Name":"Johni",' +
+     '"Sports":["football","volleyball","chess"]' +
+    '}';
+
+  CheckEqualsString( JSON, BsonToJson( d ) );
+
+end;
+
+procedure TTestBSONUtils.Test05;
+begin
+
+  d := BSON([
+    'name' , 'Johni Douglas Marangon',
+    'age' , 27,
+    'sex' , 'M',
+    'salary' , 1500,
+    'registered' , true,
+    'favorites' , BSON([ 'color' , 'Red' , 'sport' , 'Soccer', 'food' , 'Spaghetti'] )
+  ]);
+
+
+  JSON :=
+    '{"name":"Johni Douglas Marangon",' +
+     '"age":27,' +
+     '"sex":"M",' +
+     '"salary":1500,' +
+     '"registered":true,' +
+     '"favorites":{' +
+        '"color":"Red",' +
+        '"sport":"Soccer",' +
+        '"food":"Spaghetti"' +
+       '}' +
+     '}';
+
+  CheckEqualsString(JSON, BsonToJson(d));
+
+end;
+
+procedure TTestBSONUtils.TestBasic;
+begin
+  d := BSON(['name', 'Johni Douglas Marangon', 'address',
+    'Maravilia - Santa Catarina - Brazil', 'phone', 4895959859]);
+
+  JSON :=
+    '{"name":"Johni Douglas Marangon","address":"Maravilia - Santa Catarina - Brazil","phone":4895959859}';
+
+  CheckEqualsString(JSON, BsonToJson(d));
+
+
+
+  (*
+
+    "skills": [
+    {
+    "category": "PHP",
+    "tests": [
+    { "name": "One", "score": 90 },
+    { "name": "Two", "score": 96 }
+    ]
+    },
+    {
+    "category": "CouchDB",
+    "tests": [
+    { "name": "One", "score": 32 },
+    { "name": "Two", "score": 84 }
+    ]
+    },
+    {
+    "category": "Node.js",
+    "tests": [
+    { "name": "One", "score": 97 },
+    { "name": "Two", "score": 93 }
+    ]
+    }
+    ]
+    }
+*)
+end;
+
 initialization
-  RegisterTest( TTestBSONUtils.Suite );
+
+RegisterTest(TTestBSONUtils.Suite);
 
 end.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
