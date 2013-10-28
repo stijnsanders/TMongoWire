207f1a196edcbf0abb9cd5b1be4517f99b209f46
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2013-10-28 21:33:07 -0200

    rename project

---
+++ test/Test.dpr
@@ -0,0 +1,25 @@
+program Test;
+
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestBSONUtils in 'TestBSONUtils.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
